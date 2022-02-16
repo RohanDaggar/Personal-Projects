cedc63b9f7eb50b8e564d0cb15c17991a0c33354
RohanDaggar <dagzthegreat@gmail.com>
2022-02-16 03:27:24 +0000

    added a take_input and started a word comparer

--- wordle/wordle_functions.py
+++ wordle/wordle_functions.py
@@ -8,8 +8,26 @@ def load_words():
         words = [word[:5] for word in f]
     return words
 
+def take_input(list_of_words):
+    try:
+        guess = input("Enter your 5 word guess: ")
+        if guess not in list_of_words or len(guess) != 5:
+            print("Error, guess has to be a valid 5 letter word")
+            raise Exception()
+    except:
+        return take_input(list_of_words)
+    return guess.lower()
+
+def check_word(guess, actual):
+    for i in range(5):
+        print(i)
 
 if __name__ == "__main__":
     wordList = load_words()
-    print(wordList[:10])
-    print(len(wordList))
+    #print(wordList[:10])
+    #print(len(wordList))
+    
+    #guess = take_input(wordList)
+    #print(guess)
+    
+    check_word("hello", "arose")
