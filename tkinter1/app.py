bb61538be7dffd1e2c1b1260f4c135d65fbd1330
RohanDaggar <dagzthegreat@gmail.com>
2022-02-16 03:26:58 +0000

    worked a bit more on tkinter

--- tkinter1/app.py
+++ tkinter1/app.py
@@ -9,6 +9,11 @@ canvas = tk.Canvas(root, height=700, width=700, bg='#8f34eb')
 canvas.pack()
 
 frame = tk.Frame(root, bg='white')
-frame.place(relwidth=0.8, relheight=0.8)
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+openFile = tk.Button(root, text="Open File", padx=10,
+                     pady=5, fg="white", bg="#8f34eb")
+openFile.pack()
+
 
 root.mainloop()
