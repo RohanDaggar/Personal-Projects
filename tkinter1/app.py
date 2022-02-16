116171d010a7001b91b10458aea54b5d7ad05116
RohanDaggar <dagzthegreat@gmail.com>
2022-02-16 02:52:14 +0000

    added an extra box

--- tkinter1/app.py
+++ tkinter1/app.py
@@ -5,6 +5,10 @@ import os
 root = tk.Tk()
 
 
-canvas = tk.Canvas(root, height=700, width=700)
-print()
+canvas = tk.Canvas(root, height=700, width=700, bg='#8f34eb')
+canvas.pack()
+
+frame = tk.Frame(root, bg='white')
+frame.place(relwidth=0.8, relheight=0.8)
+
 root.mainloop()
