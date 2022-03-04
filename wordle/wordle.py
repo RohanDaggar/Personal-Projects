9ed7c155537b1f71a525679ccb60624df1d65744
RohanDaggar <dagzthegreat@gmail.com>
2022-03-04 19:33:02 +0000

    moved the class to another file

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -1,24 +1,15 @@
 #This is a wordle clone, but made in python
 from random import randint
-from wordle_functions import load_words, take_input, check_word
+from wordle_functions import load_words, guessWord
 
-class Cguess:
-    def __init__(self, name, gy, actualWord):
-        """initial creation of the class
 
-        Args:
-            name (str): the 5 letter guess
-            gy (list): a list of green yellow : [0,g,r,0,r] that shows how close the guess is to the word
-        """
-        self.name = name
-        self.ga = check_word(name, actualWord)
     
 
 def run():
     Totalwordlist = load_words()
     winning_word = Totalwordlist[randint(0,len(Totalwordlist))]
     print(winning_word)
-    guess = take_input(Totalwordlist)
+    guess = guessWord(Totalwordlist, winning_word)
 
 if __name__ == "__main__":
     run()
